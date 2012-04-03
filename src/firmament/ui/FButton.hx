b0b9621f09c2fcbafb7b7e3ec0b06b4e2d48a7ad
Jordan Wambaugh <jordan@wambaugh.org>
2012-04-02 18:40:33 -0700

    added basic ui elements

---
+++ src/firmament/ui/FButton.hx
@@ -0,0 +1,23 @@
+package firmament.ui;
+import nme.display.Bitmap;
+import nme.display.Sprite;
+
+/**
+ * ...
+ * @author Jordan Wambaugh
+ */
+
+class FButton extends Sprite
+{
+
+	var bitmap:Bitmap;
+	public function new(text:String) 
+	{
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/firmament/ui/FWindow.hx
@@ -0,0 +1,17 @@
+package firmament.ui;
+import nme.display.Sprite;
+
+/**
+ * ...
+ * @author Jordan Wambaugh
+ */
+
+class FWindow extends Sprite
+{
+
+	public function new() 
+	{
+		
+	}
+	
+}
