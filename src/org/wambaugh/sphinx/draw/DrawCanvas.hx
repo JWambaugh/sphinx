17319f096fdd1e5c2488ac70fb5672a5df59d81d
Jordan Wambaugh <jordan@wambaugh.org>
2012-04-02 21:50:29 -0700

    got drawing to work mostly

--- src/org/wambaugh/sphinx/draw/DrawCanvas.hx
+++ src/org/wambaugh/sphinx/draw/DrawCanvas.hx
@@ -3,6 +3,9 @@ import nme.display.Bitmap;
 import nme.display.BitmapData;
 import nme.display.Sprite;
 import nme.events.MouseEvent;
+import firmament.utils.FBitmapTracer;
+import firmament.utils.FTriangulator;
+import nme.events.Event;
 /**
  * ...
  * @author Jordan Wambaugh
@@ -11,21 +14,31 @@ import nme.events.MouseEvent;
 class DrawCanvas extends Sprite
 {
 
-	var bitmap:Bitmap;
+	public var bitmap:Bitmap;
 	var hasDrawn:Bool;
-	var startDraw: { x:Int, y:Int };
+	var startDraw: { x:Float, y:Float };
+	public static inline var DONE_DRAW='doneDraw';
 	public function new(width:Int, height:Int) 
 	{
-		this.bitmap = new Bitmap(new BitmapData(width,height));
+		this.bitmap = new Bitmap(new BitmapData(width, height,true,0));
+		
 		this.addChild(this.bitmap);
-		this.graphics.lineStyle(1,0xFF00FF);
+		this.graphics.lineStyle(1,0x0010DD);
 		this.addEventListener(MouseEvent.MOUSE_MOVE, drawTo);
 		this.addEventListener(MouseEvent.MOUSE_DOWN, function(e:MouseEvent) { 
 			this.graphics.moveTo(e.localX, e.localY);
 			this.hasDrawn = true; 
-			
+			this.startDraw = { x:e.localX, y:e.localY};
 		} );
-		this.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) { this.hasDrawn = false; } );
+		this.addEventListener(MouseEvent.MOUSE_UP, function(e:MouseEvent) { 
+			if (this.hasDrawn && this.startDraw!=null) {
+				this.graphics.lineTo(startDraw.x, startDraw.y);
+				this.bitmap.bitmapData.draw(this);
+				this.dispatchEvent(new Event(DONE_DRAW));
+			}
+			this.hasDrawn = false;
+			
+			} );
 		super();
 	}
 	
@@ -40,4 +53,23 @@ class DrawCanvas extends Sprite
 		
 	}
 	
+	
+	public function getShapes(shapeDef:Dynamic) {
+		var tracer = new FBitmapTracer(this.bitmap.bitmapData);
+		trace('done. Tracing...');
+		var poly = tracer.getPolys();
+		trace('done Tracing.');
+		trace('Vectors:'+poly.length);
+		var triangles = FTriangulator.getTriangles(poly);
+		
+		var shapes = new Array<Dynamic>() ;
+		for (triangle in triangles) {
+			var shape = Reflect.copy(shapeDef);
+			shape.type = 'polygon';
+			shape.vectors = triangle;
+			shapes.push(shape);
+		}
+		return shapes;
+		
+	}
 }
